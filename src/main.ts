9cf95aa35d2e7715dc32fee604830d290b379b20
mikelxk <mike.lxk@gmail.com>
2024-11-10 14:23:48 -0800

    update version replace http

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@
 //write a script in tampermonkey that can remove redirects in zhihu.com, https://link.zhihu.com/?target=* to * from any buttons or links on zhihu.com
 
 //add a interval to check every 1 second
-
+let prevCnt = 0
 setInterval(() => {
   let links = document.querySelectorAll("a")
-
+  let newCnt = links.length
+  if (newCnt === prevCnt) {
+    prevCnt = newCnt
+    return
+  }
+  prevCnt = newCnt
   links.forEach(link => {
-    if (link.href.includes("https://link.zhihu.com/?target=")) {
+    if (
+      link.href.includes("https://link.zhihu.com/?target=") ||
+      link.href.includes("http://link.zhihu.com/?target=")
+    ) {
       const urlParams = new URLSearchParams(link.href.split("?")[1])
       const newLink = urlParams.get("target")
       if (newLink) {
